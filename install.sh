a175886cf163b1fdec2add11e8b8f6eae0f566fd
grtw2116 <taru0116@gmail.com>
2023-01-28 02:58:54 +0900

    add: latexmkrcの設定

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 FILE="~/.config/nvim/init.vim"
-if [ -f $FILE ]; then
-	rm $FILE
-else
+if [ ! -f $FILE ]; then
 	mkdir -p ~/.config/nvim
 	ln -s ~/dotfiles/init.vim ~/.config/nvim
+fi
+
+FILE="~/.latexmkrc"
+if [ ! -f $FILE ]; then
+	ln -s ~/dotfiles/.latexmkrc ~/.latexmkrc
 fi
