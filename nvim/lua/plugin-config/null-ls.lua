cc78c3c656662f95d618615ed456a64a4dc47085
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-05-15 12:44:20 +0900

    フォーマット

--- nvim/lua/plugin-config/null-ls.lua
+++ nvim/lua/plugin-config/null-ls.lua
@@ -5,13 +5,13 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettier
     },
-    on_attach = function (client, bufnr)
+    on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr})
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
-                callback = function ()
+                callback = function()
                     vim.lsp.buf.format({ bufnr = bufnr })
                 end
             })
