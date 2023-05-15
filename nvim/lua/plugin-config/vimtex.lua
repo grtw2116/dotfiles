5fa58ad2f3c291507e93babd052ef1726c111119
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-05-15 12:44:41 +0900

    WSL環境でLaTeXのプレビューにsumatraPDFを使うように設定

--- nvim/lua/plugin-config/vimtex.lua
+++ nvim/lua/plugin-config/vimtex.lua
@@ -1,1 +1,7 @@
-vim.g.vimtex_view_method = 'skim'
+-- osがmacの場合のみ
+if vim.fn.has("mac") == 1 then
+    vim.g.vimtex_view_method = 'skim'
+else
+    vim.g.vimtex_view_general_viewer = "/mnt/c/Users/ryg-wtnb/AppData/Local/SumatraPDF/SumatraPDF.exe"
+    vim.g.vimtex_view_general_options = "--reuse-instance --forward-search @tex @line @pdf"
+end
