9337eb0c479e712648228ed731bb191e6c55dbea
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-05-22 15:53:26 +0900

    hotfix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
-require("base")                     -- 基本設定
-require("init-packer")              -- Packer.nvimの初期インストール
-require("plugin-list")              -- プラグイン設定
+require("base")                                   -- 基本設定
+require("plugin-config/init-packer")              -- Packer.nvimの初期インストール
+require("plugin-list")                            -- プラグイン設定
