2853799ef641ea992a42b33112df8af5f92a9700
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-07-11 13:30:22 +0000

    vimtexでのpdfビューア設定を変更

--- nvim/lua/plugin-config/vimtex.lua
+++ nvim/lua/plugin-config/vimtex.lua
@@ -1,7 +1,9 @@
 -- osがmacの場合のみ
 if vim.fn.has("mac") == 1 then
     vim.g.vimtex_view_method = 'skim'
-else
+elseif vim.fn.has("win32") == 1 then
     vim.g.vimtex_view_general_viewer = "/mnt/c/Users/ryg-wtnb/AppData/Local/SumatraPDF/SumatraPDF.exe"
     vim.g.vimtex_view_general_options = "--reuse-instance --forward-search @tex @line @pdf"
+elseif vim.fn.has("linux") == 1 then
+    vim.g.vimtex_view_general_viewer = 'zathura'
 end
