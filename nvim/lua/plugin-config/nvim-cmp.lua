7688b67aa9bd4148f8b2eece8298ccb248da6853
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-07-27 16:52:33 +0900

    cmpのリソースを追加

--- nvim/lua/plugin-config/nvim-cmp.lua
+++ nvim/lua/plugin-config/nvim-cmp.lua
@@ -1,6 +1,10 @@
 local cmp = require("cmp")
 
 cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     snippet = {
         expand = function(args)
             require("luasnip").lsp_expand(args.body)
@@ -9,10 +13,12 @@ cmp.setup({
     sources = {
         { name = "nvim_lsp" },
         { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lsp_document_symbol" },
         { name = "buffer" },
         { name = "path" },
         { name = "nvim_lua" },
         { name = "luasnip" },
+        { name = "calc" },
     },
     mapping = cmp.mapping.preset.insert({
         ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -20,5 +26,20 @@ cmp.setup({
         ["<C-l>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm { select = true },
+    }),
+    formatting = {
+        format = require("lspkind").cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+        })
+    },
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+        })
     })
 })
