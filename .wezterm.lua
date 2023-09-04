63cf243149696ca99b3c12cfb41386da7e648c5f
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-09-04 18:39:18 +0900

    weztermの設定ファイルを追加

---
+++ .wezterm.lua
@@ -0,0 +1,32 @@
+-- wezterm APIからpull
+local wezterm = require("wezterm")
+
+-- このテーブルに設定項目を追加していく
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+---------------------
+-- 設定記述開始
+---------------------
+
+config.font = wezterm.font_with_fallback({
+    "PlemolJP Console NF"
+})
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- config.font_size = 12.0
+
+---------------------
+-- 設定記述終了
+---------------------
+
+return config
