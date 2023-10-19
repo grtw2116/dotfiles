6b63b552cb30441e05107b39a721ca0eac4312ff
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-10-19 15:03:22 +0900

    neovimのインストール設定を追加

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -3,5 +3,10 @@ sudo apt upgrade -y
 sudo apt install -y \
     bat \
     build-essential \
-    zsh \
+    software-properties-common \
+    zsh
 
+# Neovimのインストール
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install -y neovim
