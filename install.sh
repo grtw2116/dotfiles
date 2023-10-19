655cb30acb2dccd716d57fa75d79a08f96e74d78
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-10-19 13:15:48 +0900

    if文が誤っていた問題を修正

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/bash
+
 # dotfilesのレポジトリがあるパスを取得
 export DOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # OSごとの処理
 kernel_name=$(uname)
-if ["${kernel_name}" == "Darwin"]; then
+if [ ${kernel_name} = "Darwin" ]; then
     $DOT_DIR/scripts/macos.sh
-elif [ "${kernel_name}" == "Linux" ]; then
+elif [ ${kernel_name} = "Linux" ]; then
     $DOT_DIR/scripts/linux.sh
 fi
 
