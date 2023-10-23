732d371c358df241cb58d31fcef10969dab32ca9
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-10-23 11:51:02 +0900

    <C-b>でバッファ内を検索できるように

--- config/nvim/lua/plugin-config/telescope.lua
+++ config/nvim/lua/plugin-config/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})     -- <C-p>: ファイル名から検索
 vim.keymap.set("n", "<C-g>", builtin.live_grep, {})       -- <C-g>: ファイル内の文字列から検索
+vim.keymap.set("n", "<C-b>", builtin.buffers, {})       -- <C-b>: バッファを検索
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})   -- <leader>fb: バッファを検索
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {}) -- <leader>fh: タグを検索
 
