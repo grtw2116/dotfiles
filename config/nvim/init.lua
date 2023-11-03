336c26ba229078f6cc4978e22ab20ec1c43b450a
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-11-03 19:04:24 +0900

    neovimのバッファ関連のキーマップを設定

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("base")                                   -- 基本設定
 require("terminal")                               -- ターミナル設定
+require("buffer")                                 -- バッファ設定
 require("plugin-config/init-lazy")                -- Lazy.nvimの初期インストール
 require("plugin-list")                            -- プラグイン設定
---
+++ config/nvim/lua/buffer.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "b[", ":bprevious<CR>", { silent = true })
+vim.keymap.set("n", "b]", ":bnext<CR>", { silent = true })
+vim.keymap.set("n", "B[", ":bfirst<CR>", { silent = true })
+vim.keymap.set("n", "B]", ":blast<CR>", { silent = true })
