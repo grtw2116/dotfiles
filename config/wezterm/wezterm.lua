df1905e7e4e2005234f34a4ef7869bf18a30d7be
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-11-30 12:44:14 +0900

    自動で配色を変更

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,40 +5,55 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 ---------------------
 -- 設定記述開始
 ---------------------
 
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Tokyo Night"
+	else
+		return "Tokyo Night Day"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
 config.font = wezterm.font_with_fallback({
-    "PlemolJP Console NF"
+	"PlemolJP Console NF",
 })
 
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.font_size = 15.0
-config.color_scheme = "tokyonight"
 
 -- <C-q>: Leaderキー
 config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 2000 }
 
 local act = wezterm.action
 config.keys = {
-    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection 'Left' },
-    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection 'Down' },
-    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection 'Up' },
-    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection 'Right' },
-    { key = "-", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-    { key = "|", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } }
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 }
-
 
 -- ssh設定
 -- .ssh/configに設定したホスト名を指定する
