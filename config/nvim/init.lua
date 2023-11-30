60e5fd4ffcc9d151ce01991c37240735b2351c1b
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2023-11-30 12:22:27 +0900

    バッファ設定を削除

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,4 @@
 require("base")                                   -- 基本設定
 require("terminal")                               -- ターミナル設定
-require("buffer")                                 -- バッファ設定
 require("plugin-config/init-lazy")                -- Lazy.nvimの初期インストール
 require("plugin-list")                            -- プラグイン設定
--- config/nvim/lua/buffer.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "b[", ":bprevious<CR>", { silent = true })
-vim.keymap.set("n", "b]", ":bnext<CR>", { silent = true })
-vim.keymap.set("n", "B[", ":bfirst<CR>", { silent = true })
-vim.keymap.set("n", "B]", ":blast<CR>", { silent = true })
