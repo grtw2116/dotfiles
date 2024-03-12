4f42f4b79cd63da11f68b0444660ceb1f4fa9643
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-03-12 12:22:21 +0900

    LSPのアイコン表示を変更

--- config/nvim/lua/plugin-config/lspconfig.lua
+++ config/nvim/lua/plugin-config/lspconfig.lua
@@ -1,16 +1,16 @@
 -- --------------------------------------------------------------
 -- アイコン設定
 -- --------------------------------------------------------------
-local signs = {
-	Error = " ",
-	Warn = " ",
-	Hint = " ",
-	Info = " ",
-}
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+-- local signs = {
+-- 	Error = " ",
+-- 	Warn = " ",
+-- 	Hint = " ",
+-- 	Info = " ",
+-- }
+-- for type, icon in pairs(signs) do
+-- 	local hl = "DiagnosticSign" .. type
+-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+-- end
 
 -- --------------------------------------------------------------
 -- LSPサーバ読み込み
