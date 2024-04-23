6d3ab4ced5939e6806e6358ad2a9280061403469
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-04-23 23:26:56 +0900

    sketchybarの設定を追加

--- config/sketchybar/plugins/battery.sh
+++ config/sketchybar/plugins/battery.sh
@@ -7,20 +7,20 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+case ${PERCENTAGE} in
+  9[0-9]|100) ICON="􀛨"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="􀺸"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="􀺶"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="􀛩"
   ;;
-  *) ICON=""
+  *) ICON="􀛪"
 esac
 
-if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+if [[ $CHARGING != "" ]]; then
+  ICON="􀢋"
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
---
+++ config/sketchybar/plugins/calendar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sketchybar --set $NAME label="$(LANG=ja_JP.UTF-8 date +'%b月%d日 (%a) %H:%M')"
