001fa2b3cc8b7e3eee250b5332edb1a3b28f379d
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-10-19 17:49:34 +0900

    nvim-cmpを復活

--- config/nvim/lua/plugins/lsp/mason-tool-installer.lua
+++ config/nvim/lua/plugins/lsp/mason-tool-installer.lua
@@ -1,26 +1,26 @@
 return {
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "texlab",
-                "rust_analyzer",
-                "bashls",
-                "html",
-                "cssls",
-                "pyright",
-                "jsonls",
-                "dockerls",
-                "docker_compose_language_service",
-                "gradle_ls",
-                "tailwindcss",
-                "eslint",
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = "williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"texlab",
+				"rust_analyzer",
+				"bashls",
+				"html",
+				"cssls",
+				"pyright",
+				"jsonls",
+				"dockerls",
+				"docker_compose_language_service",
+				"gradle_ls",
+				"tailwindcss",
+				-- "eslint",
 
-                -- Lua
-                "lua_ls",
-                "stylua",
-            }
-        }
-    }
+				-- Lua
+				"lua_ls",
+				"stylua",
+			},
+		},
+	},
 }
