730a52cf8810a4c6a717f38e1ff44d98ccee9418
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-11-13 01:36:27 +0900

    カンマピリオドを句読点に変換するキーマップを追加

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,3 +1,6 @@
 -- バッファ間の移動
 vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { noremap = true, silent = true })
+
+-- ユーティリティ
+vim.keymap.set("n", "<leader>rp", "[[<cmd>%s/，/、/g | %s/．/。/g<cr>]]", { noremap = true, silent = true })
