93d6f4c9b3cd9c255b112979a6c177eee2238325
grtw2116 <15563588+grtw2116@users.noreply.github.com>
2024-12-19 17:04:39 +0900

    feat: add debug plugin

--- config/nvim/lua/plugin.lua
+++ config/nvim/lua/plugin.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,5 +18,6 @@ require("lazy").setup({
 	{ import = "plugins.editor" },
 	{ import = "plugins.lang" },
 	{ import = "plugins.lsp" },
+	{ import = "plugins.dap" },
 	{ import = "plugins.ui" },
 })
